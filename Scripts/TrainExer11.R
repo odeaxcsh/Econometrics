f622e4d2350f7aa83cce68dba1bd9c20ff091f21
Abolfazl Babanazari <odaat.iaath@gmail.com>
2022-03-29 13:29:55 +0430

    Training Exercise 1.1

---
+++ Scripts/TrainExer11.R
@@ -0,0 +1,43 @@
+df <- read.delim("Datasets/TrainExer11.txt", header = T, sep = "\t")
+
+hist(df$Age, xlab = "Age",
+  main = "Histogram for Ages"
+)
+
+hist(
+  df$Expenditures, xlab = "Expenditures",
+  main = "Histogram for Expenditures"
+)
+
+plot(df$Age, df$Expenditures,
+  main = "Scatter Plot of Expenditures/Age",
+  xlab = "Age",
+  ylab = "Expenditures"
+)
+
+print(sprintf("the sample mean of expenditures of all 26 clients is %f",
+    mean(df$Expenditures)
+))
+
+youngers <- df[df$Age < 40, ]
+olders <- df[df$Age >= 40, ]
+
+print(sprintf(
+  "The sample mean of younger clients is %f and for olders is %f",
+  mean(youngers$Expenditures),
+  mean(olders$Expenditures)
+))
+
+ymodel <- lm(Expenditures ~ Age, data = youngers)
+
+y25exp <- predict(ymodel, data.frame(Age = c(25)))
+
+print(sprintf("We expect a 25 years old expenditures to be about %f", y25exp))
+
+
+omodel <- lm(Expenditures ~ Age, data = olders)
+
+o50exp <- predict(ymodel, data.frame(Age = c(50)))
+
+print(sprintf("We expect a 50 years old expenditures to be about %f", o50exp))
+
